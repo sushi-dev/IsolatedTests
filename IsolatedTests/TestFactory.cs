30069593863682ef9c5a2d6ef4cb5d6047b374f6
James Prompanya <ch0p57ickz@users.noreply.github.com>
2016-05-30 10:57:56 -0700

    Add support for non-default constructors

--- IsolatedTests/TestFactory.cs
+++ IsolatedTests/TestFactory.cs
@@ -17,10 +17,10 @@ namespace Sushi.IsolatedTests
             this.appDomain = AppDomain.CreateDomain("Test Factory", null, appDomainSetup);
         }
 
-        public TestBase CreateTest<T>() where T : TestBase
+        public TestBase CreateTest<T>(params object[] args) where T : TestBase
         {
             var type = typeof(T);
-            return (TestBase)this.appDomain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
+            return (TestBase)this.appDomain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName, false, 0, null, args, null, null);
         }
 
         public void Dispose()
