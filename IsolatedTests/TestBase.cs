d79638e12f4709e8e06267c743b8b9d06d43fd7b
ch0p57ickz <jrprompanya@ucla.edu>
2016-05-30 10:25:16 -0700

    Initial commit

---
+++ IsolatedTests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sushi.IsolatedTests
+{
+    using System;
+    using System.Diagnostics;
+
+    public abstract class TestBase : MarshalByRefObject
+    {
+        public abstract void Execute();
+
+        public TimeSpan RunTest()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            this.Execute();
+            stopwatch.Stop();
+
+            return stopwatch.Elapsed;
+        }
+    }
+}
---
+++ IsolatedTests/TestFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sushi.IsolatedTests
+{
+    public sealed class TestFactory : IDisposable
+    {
+        private AppDomain appDomain;
+
+        public TestFactory()
+        {
+            var appDomainSetup = new AppDomainSetup { ApplicationBase = AppDomain.CurrentDomain.BaseDirectory };
+
+            this.appDomain = AppDomain.CreateDomain("Test Factory", null, appDomainSetup);
+        }
+
+        public TestBase CreateTest<T>() where T : TestBase
+        {
+            var type = typeof(T);
+            return (TestBase)this.appDomain.CreateInstanceAndUnwrap(type.Assembly.FullName, type.FullName);
+        }
+
+        public void Dispose()
+        {
+            if (this.appDomain != null)
+            {
+                AppDomain.Unload(this.appDomain);
+            }
+        }
+    }
+}
